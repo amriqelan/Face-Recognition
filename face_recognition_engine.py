57f3d41208bf09f4317a9f051128b2e279239909
Amr Iqelan <116590634+amriqelan@users.noreply.github.com>
2023-05-27 14:58:33 +0300

    upload files

--- face_recognition_engine.py
+++ face_recognition_engine.py
@@ -2,36 +2,61 @@ import face_recognition
 import cv2
 import os
 import glob
+import csv
 import numpy as np
 
-class FacialRecognizer:
+class SimpleFacerec:
     def __init__(self):
         self.known_face_encodings = []
         self.known_face_names = []
-
-        # Resize frame for faster processing
         self.frame_resizing = 0.25
 
+        # Create directory to store face encodings if it doesn't exist
+        self.face_encodings_dir = "face_encodings"
+        if not os.path.exists(self.face_encodings_dir):
+            os.makedirs(self.face_encodings_dir)
+
     def load_encoding_images(self, images_path):
-        # Load images
         images_path = glob.glob(os.path.join(images_path, "*.*"))
         print("{} encoding images found.".format(len(images_path)))
 
-        # Store image encodings and names
         for img_path in images_path:
             img = cv2.imread(img_path)
             rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # Get the filename only from the initial file path.
             basename = os.path.basename(img_path)
             (filename, ext) = os.path.splitext(basename)
-            # Get encoding
+
             img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-            # Store file name and file encoding
             self.known_face_encodings.append(img_encoding)
             self.known_face_names.append(filename)
+
         print("Encoding images loaded")
+
+    def load_encoding_data(self):
+        data_file = os.path.join(self.face_encodings_dir, "face_data.csv")
+        if not os.path.exists(data_file):
+            return
+
+        with open(data_file, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                name = row[0]
+                encoding = np.array(row[1:], dtype=float)
+                self.known_face_names.append(name)
+                self.known_face_encodings.append(encoding)
+
+    def save_encoding_data(self):
+        data_file = os.path.join(self.face_encodings_dir, "face_data.csv")
+
+        with open(data_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for name, encoding in zip(self.known_face_names, self.known_face_encodings):
+                row = [name] + encoding.tolist()
+                writer.writerow(row)
+
+        print("Face encoding data saved")
 
     def detect_known_faces(self, frame):
         small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
@@ -43,13 +68,13 @@ class FacialRecognizer:
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
             name = "Unknown"
+
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
             face_names.append(name)
 
-        # Convert to numpy array to adjust coordinates with frame resizing quickly
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resizing
         return face_locations.astype(int), face_names
@@ -58,10 +83,5 @@ class FacialRecognizer:
         rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-        # Store the new face encoding and name
         self.known_face_encodings.append(img_encoding)
         self.known_face_names.append(name)
-
-    def clear_encodings(self):
-        self.known_face_encodings.clear()
-        self.known_face_names.clear()
--- gui_face_recognition.py
+++ gui_face_recognition.py
@@ -1,7 +1,8 @@
 import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
-from face_recognition_engine import FacialRecognizer
+from face_recognition_engine import SimpleFacerec
+import os
 
 
 class FaceRecognitionApp:
@@ -9,14 +10,17 @@ class FaceRecognitionApp:
         self.root = root
         self.root.title("Face Recognition App")
 
-        # Create instance of FacialRecognizer
-        self.sfr = FacialRecognizer()
-        self.sfr.load_encoding_images("images/")
+        self.sfr = SimpleFacerec()
+        # Create the face_encodings directory if it doesn't exist
+        face_encodings_dir = "face_encodings"
+        if not os.path.exists(face_encodings_dir):
+            os.makedirs(face_encodings_dir)
 
-        # Create video capture
+        self.sfr.load_encoding_images("images/")
+        self.sfr.load_encoding_data()
+
         self.cap = cv2.VideoCapture(0)
 
-        # Create GUI elements
         self.video_label = tk.Label(root)
         self.video_label.pack()
 
@@ -29,60 +33,43 @@ class FaceRecognitionApp:
         self.quit_button = tk.Button(root, text="Quit", command=self.quit)
         self.quit_button.pack()
 
-        # Start video stream
         self.update_video()
 
     def update_video(self):
         ret, frame = self.cap.read()
 
-        # Detect faces
         face_locations, face_names = self.sfr.detect_known_faces(frame)
 
-        # Draw rectangles and names on the frame
         for face_loc, name in zip(face_locations, face_names):
             y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 4)  # Green rectangle
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 4)
             cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 200), 2, cv2.LINE_AA)
 
-        # Convert the frame to ImageTk format
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(img)
         img = ImageTk.PhotoImage(image=img)
 
-        # Update the video label
         self.video_label.configure(image=img)
         self.video_label.image = img
 
-        # Repeat the update after a delay (30 milliseconds)
         self.root.after(30, self.update_video)
 
     def capture_face(self):
-        # Capture an image from the video feed
         ret, frame = self.cap.read()
-
-        # Display the captured image
         cv2.imshow("Capture", frame)
-
-        # Prompt the user to enter a name for the captured face
         name = self.name_entry.get()
 
-        # Save the captured face to the recognition database
         self.sfr.add_encoding(frame, name)
 
-        # Clear the name entry field
         self.name_entry.delete(0, tk.END)
 
     def quit(self):
+        self.sfr.save_encoding_data()
         self.cap.release()
         cv2.destroyAllWindows()
         self.root.destroy()
 
 
-# Create the Tkinter root window
 root = tk.Tk()
-
-# Create an instance of the FaceRecognitionApp
 app = FaceRecognitionApp(root)
-
-# Start the Tkinter event loop
 root.mainloop()
